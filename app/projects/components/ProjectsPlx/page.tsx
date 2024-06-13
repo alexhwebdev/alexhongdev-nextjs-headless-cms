c23d4d123cc5036a2fe72d8fdda0686798760e6a
Alex Hong <alexhdevmmini@Alexs-Mac-mini.lan>
2024-06-12 14:19:10 -1000

    SOLVED : Multiple GSAP scroller issue

--- app/projects/components/ProjectsPlx/page.tsx
+++ app/projects/components/ProjectsPlx/page.tsx
@@ -11,12 +11,36 @@ import { raleway } from '@/app/fonts';
 import { inter } from '@/app/fonts';
 import './page.scss';
 
+
+const projectsList = [
+  { 
+    title: "How to Invest in AI",
+    description: "Artificial Intelligence, or AI, has the possibility to usher in...",
+    imgSrc: "ibd-ai.png",
+    gifSrc: "ibd-ai.gif",
+  },
+  { 
+    title: "How to Invest in AI",
+    description: "Artificial Intelligence, or AI, has the possibility to usher in...",
+    imgSrc: "ibd-ai.png",
+    gifSrc: "ibd-ai.gif",
+  },
+  { 
+    title: "How to Invest in AI",
+    description: "Artificial Intelligence, or AI, has the possibility to usher in...",
+    imgSrc: "ibd-ai.png",
+    gifSrc: "ibd-ai.gif",
+  },
+]
+
+
 // const ProjectPlx = () => {
 export default function ProjectPlx() {
   // const vidRef = useRef();
   // useEffect(() => { vidRef.current.play(); },[]);
-  const title1 = useRef(null);
-  const container = useRef(null);
+  
+  const titleContainerRef = useRef(null);
+  const titleRef = useRef(null);
   
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -48,65 +72,196 @@ export default function ProjectPlx() {
       })
     })
   }, []);
+  
+  // useLayoutEffect( () => {
+  //   const context = gsap.context( () => {
+  //     const tl = gsap.timeline({
+  //       scrollTrigger: {
+  //         trigger: titleContainerRef.current,
+  //         start: "10% 50%",
+  //         end: "50%+=100 0%",
+  //         // start: "100%+=2000 50%", // start, scroller-start
+  //         // end: "100%+=2000 10%",   // end, scroller-end
+  //         scrub: true,
+  //         markers: true,
+  //       },
+  //     })
+  //     .to(titleRef.current, {y: -100}, 0)
+  //   })
+  //   return () => context.revert();
+  // }, [])
 
   useLayoutEffect( () => {
-    const context = gsap.context( () => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: container.current,
-          start: "10% 50%",
-          end: "50%+=100 0%",
-          // start: "100%+=2000 50%", // start, scroller-start
-          // end: "100%+=2000 10%",   // end, scroller-end
-          scrub: true,
-          markers: true,
-        },
-      })
-      .to(title1.current, {y: -100}, 0)
-    })
-    return () => context.revert();
+    // const context = gsap.context( () => {
+      gsap.utils
+        .toArray<HTMLElement>("[data-module-parallax]")
+        .forEach((section) => {
+          const parallax = section.querySelector("[data-parallax]");
+
+          gsap.fromTo(
+            parallax,
+            { y: 0 },
+            {
+              y: -100,
+              ease: "none",
+              scrollTrigger: {
+                trigger: section,
+                start: "10% 50%",
+                end: "50%+=100 0%",
+                scrub: true,
+                markers: true
+              }
+            }
+          );
+
+          // gsap.timeline({
+          //   scrollTrigger: {
+          //     trigger: section,
+          //     start: "10% 50%",
+          //     end: "50%+=100 0%",
+          //     scrub: true,
+          //     markers: true,
+          //   }
+          // })
+          // .to(section!, { y: -100 }, 0);
+        });
+
+      // const eachTitle = gsap.utils.toArray(".each_title");
+      // eachTitle.forEach(title => {
+      //   gsap.timeline({
+      //     scrollTrigger: {
+      //       trigger: titleContainerRef.current,
+      //       start: "10% 50%",
+      //       end: "50%+=100 0%",
+      //       scrub: true,
+      //       markers: true,
+      //     }
+      //   })
+      //   .to(title!, { y: -100 }, 0);
+      // });
+
+    // })
+    // return () => context.revert();
   }, [])
+
 
   return (
 
     <div className="home">
-      <div className="project_section_wrapper">
-        <div 
-          className={`project_title ${montserrat.className}`}
-          ref={container}
-        >
-          <p
-            ref={title1}
-          >How to<br></br>Invest<br></br>in AI</p>
-        </div>
-        <div className={`project_desc ${montserrat.className}`}>
-          <p>Artificial Intelligence, or AI, has the possibility to usher in... </p>
-        </div>
-        <div className="project">
-          <div className="project_image">
-            <Image 
-              className="img" 
-              src={'/images/projects/ibd-ai.png'} 
-              // fill={true}
-              alt={"image"}
-              // placeholder="blur"
-              width={1000}
-              height={1000}
-              // sizes="(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw"
-            />
-            <div className="mask"></div>
-          </div>
+      {
+        projectsList.map((project, index) => {
+          return (
+            <div className="project_section_wrapper" data-module-parallax
+              key={index} 
+            >
+              <div 
+                className={`project_title ${montserrat.className}`}
+                ref={titleContainerRef}
+              >
+                <p 
+                  // className={'each_title'}
+                  className="each_title"
+                  ref={titleRef}
+                  data-parallax
+                >
+                  { project.title }
+                  {/* How to<br></br>Invest<br></br>in AI */}
+                </p>
+              </div>
+              <div className={`
+                project_desc ${montserrat.className}
+                `}
+              >
+                <p>
+                  { project.description }
+                  {/* Artificial Intelligence, or AI, has the possibility to usher in... */}
+                </p>
+              </div>
+              <div className="project">
+                <div className="project_image">
+                  <Image 
+                    className="img" 
+                    src={`/images/projects/${project.imgSrc}`} 
+                    // fill={true}
+                    alt={"image"}
+                    // placeholder="blur"
+                    width={1000}
+                    height={1000}
+                    // sizes="(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw"
+                  />
+                  <div className="mask"></div>
+                </div>
 
-          <Image 
-              className="gif" 
-              src={'/images/projects/ibd-ai.gif'} 
-              // fill={true}
-              alt={"gif"}
-              // placeholder="blur"
-              width={300}
-              height={250}
-              // sizes="(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw"
-            />
+                <Image 
+                    className="gif" 
+                    src={`/images/projects/${project.gifSrc}`} 
+                    // fill={true}
+                    alt={"gif"}
+                    // placeholder="blur"
+                    width={300}
+                    height={250}
+                    // sizes="(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw"
+                  />
+              </div>
+            </div>            
+          )
+        })
+      }
+    </div>
+  )
+}
+
+// export default ProjectPlx;
+// https://codepen.io/drummond-dev/pen/LYzzxrK
+
+
+
+
+
+
+
+// <div className="project_section_wrapper">
+//   <div 
+//     className={`project_title ${montserrat.className}`}
+//     ref={titleContainerRef}
+//   >
+//     <p
+//       ref={titleRef}
+//     >How to<br></br>Invest<br></br>in AI</p>
+//   </div>
+//   <div className={`project_desc ${montserrat.className}`}>
+//     <p>Artificial Intelligence, or AI, has the possibility to usher in... </p>
+//   </div>
+//   <div className="project">
+//     <div className="project_image">
+//       <Image 
+//         className="img" 
+//         src={'/images/projects/ibd-ai.png'} 
+//         // fill={true}
+//         alt={"image"}
+//         // placeholder="blur"
+//         width={1000}
+//         height={1000}
+//         // sizes="(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw"
+//       />
+//       <div className="mask"></div>
+//     </div>
+
+//     <Image 
+//         className="gif" 
+//         src={'/images/projects/ibd-ai.gif'} 
+//         // fill={true}
+//         alt={"gif"}
+//         // placeholder="blur"
+//         width={300}
+//         height={250}
+//         // sizes="(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw"
+//       />
+//   </div>
+// </div>
+
+
+
 
           {/* <video className={`video`}
             // ref={ vidRef }
@@ -120,16 +275,3 @@ export default function ProjectPlx() {
               type="video/mp4" 
             />
           </video> */}
-        </div>
-      </div>
-
-      
-
-    </div>   
-
-
-  )
-}
-
-// export default ProjectPlx;
-// https://codepen.io/drummond-dev/pen/LYzzxrK
