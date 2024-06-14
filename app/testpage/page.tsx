f90f86639c60f9b613028e024208bae40ee63986
Alex Hong <alexhdevmmini@Alexs-Mac-mini.lan>
2024-06-13 15:24:42 -1000

    testpage live test - 2

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from '@gsap/react';
@@ -10,74 +9,136 @@ import { montserrat } from '@/app/fonts';
 import { inter } from '@/app/fonts';
 
 import styles from './page.module.css'
-// import './page.css'
+import './page.css'
 
 
 
 export default function ProjectHoriPlxTest() {
-  const sectionRef = useRef(null);
   const triggerRef = useRef(null);
+  const sectionRef = useRef(null);
 
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
+
+    let mm = gsap.matchMedia(),
+        breakPoint = 800;
     
-    const pin = gsap.fromTo(
-      sectionRef.current,
-      {
-        translateX: 0,
-      },
-      {
-        translateX: "-600vw",
-        ease: "none",
-        duration: 1,
-        scrollTrigger: {
-          trigger: triggerRef.current,
-          start: "top top",
-          end: "3000 top",    // This allows horizontal scroll
-          // end: "top top",
-          scrub: 0.6,
-          pin: true,
-        },
+    mm.add({
+      // set up any number of arbitrarily-named conditions. The function below will be called when ANY of them match.
+      isDesktop: `(min-width: ${breakPoint}px) and (prefers-reduced-motion: no-preference)`,
+      isMobile: `(max-width: ${breakPoint - 1}px) and (prefers-reduced-motion: no-preference)`
+    }, (context) => {
+      // context.conditions has a boolean property for each condition defined above indicating if it's matched or not.
+      let { isDesktop, isMobile } = context.conditions,
+          target = isDesktop ? ".desktop" : ".mobile",
+          tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: ".gray",
+              scrub: 1,
+              end: "200%",
+              pin: true
+            }
+          });
+      tl.to(target, {scale: 2, rotation: 360})
+        .to(target, {scale: 1});
+    
+      // works for non-ScrollTrigger animations too: 
+      gsap.to(target, {backgroundColor: "#2c7ad2", duration: 0.8, ease: "none", repeat: -1, yoyo: true});
+    
+      return () => { 
+        // optionally return a cleanup function that will be called when the media query no longer matches
       }
-    );
-    return () => {
-      {/* A return function for killing the animation on component unmount */ }
-      pin.kill();
-    };
+    }); 
+      
+
+
+
+
+
+
+
+
+
+
+
+
+    // const pin = gsap.fromTo(
+    //   sectionRef.current,
+    //   {
+    //     translateX: 0,
+    //   },
+    //   {
+    //     translateX: "-600vw",
+    //     ease: "none",
+    //     duration: 1,
+    //     scrollTrigger: {
+    //       trigger: triggerRef.current,
+    //       start: "top top",
+    //       end: "3000 top",    // This allows horizontal scroll
+    //       // start: "100%+=2000 50%", // start, scroller-start
+    //       // end: "100%+=2000 10%",   // end, scroller-end
+    //       scrub: 0.6,
+    //       pin: true,
+    //       markers: true
+    //     },
+    //   }
+    // );
+    // return () => {
+    //   {/* A return function for killing the animation on component unmount */ }
+    //   pin.kill();
+    // };
   }, []);
 
 
   return (
-    <section className={styles.project_hori_plx_wrapper}>
-      {/* The section up act just as a wrapper. If the trigger (below) is the
-      first jsx element in the component, you get an error on route change */}
+    <div>
+      <header>
+        <h1>gsap.matchMedia()</h1>
+        <p className="lead">When the viewport is less than 800px, the "Mobile" &lt;div&gt; will animate. Otherwise, "Desktop" will.</p>
+      </header>
+      <section className="gray">
+        <div className="mobile">Mobile</div>
+        <div className="desktop">Desktop</div>
+      </section>
 
-      {/* The div below act just as a trigger. As the doc suggests, the trigger and 
-      the animation should alway be two separated refs */}
-      <div ref={triggerRef}>
-        <div className={styles.project_hori_plx_container}
-          ref={sectionRef}
-        >
-          <div className={`
-            ${styles.section_one}
-          `}>
-            <h3>Section 1</h3>
-          </div>
+      <section className="bottom lead">
+        <p><strong>Pretty cool, right?</strong></p>
+        <p>Resize your screen. 800px is the break point. It's all dynamic!</p>
+        <p>Check out <a href="https://greensock.com">GSAP</a> today. </p>
+      </section>
+    </div>
 
-          <div className={`
-            ${styles.section_two}
-          `}>
-            <h3>Section 2</h3>
-          </div>
 
-          <div className={`
-            ${styles.section_three}
-          `}>
-            <h3>Section 3</h3>
-          </div>
+    // <section className={styles.project_hori_plx_wrapper}>
+    //   {/* The section up act just as a wrapper. If the trigger (below) is the
+    //   first jsx element in the component, you get an error on route change */}
 
-        </div>
-      </div>
-    </section>
+    //   {/* The div below act just as a trigger. As the doc suggests, the trigger and 
+    //   the animation should alway be two separated refs */}
+    //   <div ref={triggerRef}>
+    //     <div className={styles.project_hori_plx_container}
+    //       ref={sectionRef}
+    //     >
+    //       <div className={`
+    //         ${styles.section_one}
+    //       `}>
+    //         <h3>Section 1</h3>
+    //       </div>
+
+    //       <div className={`
+    //         ${styles.section_two}
+    //       `}>
+    //         <h3>Section 2</h3>
+    //       </div>
+
+    //       <div className={`
+    //         ${styles.section_three}
+    //       `}>
+    //         <h3>Section 3</h3>
+    //       </div>
+
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
