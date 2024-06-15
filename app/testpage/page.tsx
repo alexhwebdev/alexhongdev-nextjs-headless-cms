05c03f5a4452671e289e5c0c1ffb31b1eb04982a
Alex Hong <alexhdevmmini@Alexs-Mac-mini.lan>
2024-06-14 16:40:35 -1000

    UPDATED : testpage - 2

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -72,7 +72,7 @@ export default function ProjectHoriPlxTest() {
             // end: "100%+=2000 10%",   // end, scroller-end
             scrub: 0.6,
             pin: true,
-            markers: true
+            // markers: true
           },
         }
       );
@@ -100,7 +100,7 @@ export default function ProjectHoriPlxTest() {
             // end: "100%+=2000 10%",   // end, scroller-end
             scrub: 0.6,
             pin: true,
-            markers: true
+            // markers: true
           },
         }
       );
