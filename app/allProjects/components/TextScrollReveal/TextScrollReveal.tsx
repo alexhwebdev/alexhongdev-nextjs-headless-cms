73f77da5c13fb85ca5c618c6093f742b5bbabb28
Alex Hong <alexhdevmmini@Alexs-Mac-mini.lan>
2024-06-28 22:37:27 -1000

    UPDATED : TextScrollReveal - 2

--- app/allProjects/components/TextScrollReveal/TextScrollReveal.tsx
+++ app/allProjects/components/TextScrollReveal/TextScrollReveal.tsx
@@ -1,67 +1,70 @@
-"use client";
-import { useRef, useEffect, MutableRefObject } from 'react';
-import gsap from 'gsap';
+'use client';
+import styles from './style.module.scss'
+import { useRef, useEffect, JSX } from 'react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import styles from './style.module.scss';
+import gsap from 'gsap';
+import { companyInfoDescProps } from '../../page';
 
-interface Props {
-  companyDesc: string;
-}
+// const phrase = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.";
 
-const TextScrollReveal: React.FC<Props> = ({ companyDesc }) => {
-  let refs = useRef<Array<MutableRefObject<HTMLElement | null>>>([]);
-  const container = useRef<HTMLDivElement | null>(null);
+export default function TextScrollReveal({companyDesc}: companyInfoDescProps) {
 
-  useEffect(() => {
+  let refs = useRef<HTMLSpanElement[]>([]);
+  const description = useRef(null);
+  const container = useRef(null);
+
+  useEffect( () => {
     gsap.registerPlugin(ScrollTrigger);
     createAnimation();
-  }, []);
+  }, [])
 
   const createAnimation = () => {
-    gsap.to(refs.current.map(ref => ref.current), {
+    gsap.to(refs.current, {
       scrollTrigger: {
-        trigger: container.current!,
+        trigger: container.current,
         scrub: true,
+        // start: "100%+=2000 50%", // start, scroller-start
+        // end: "100%+=2000 10%",   // end, scroller-end
         start: "5% 50%",
         end: `+=${window.innerHeight / 1.5}`,
-        markers: true
+        // markers: true
       },
       opacity: 1,
       ease: "none",
       stagger: 0.1
-    });
-  };
+    })
+  }
 
   const splitWords = (companyDesc: string) => {
     let description: JSX.Element[] = [];
 
-    companyDesc.split(" ").forEach((word, i) => {
+    companyDesc.split(" ").forEach( (word, i) => {
       const letters = splitLetters(word);
-      description.push(<p key={word + "_" + i}>{letters}</p>);
-    });
-
+      description.push(<p key={word + "_" + i}>{letters}</p>)
+    })
+    console.log('description ', description)
     return description;
-  };
-
+  }
+  
   const splitLetters = (word: string) => {
     let letters: JSX.Element[] = [];
-    word.split("").forEach((letter, i) => {
-      const ref: MutableRefObject<HTMLSpanElement | null> = useRef(null);
+    word.split("").forEach( (letter, i) => {
       letters.push(
-        <span key={letter + "_" + i} ref={ref}>{letter}</span>
-      );
-      refs.current.push(ref);
-    });
+        <span 
+          key={letter + "_" + i} 
+          ref={(el: HTMLSpanElement | null) => {if (el) refs.current.push(el)}}
+        >{letter}</span>)
+    })
     return letters;
-  };
+  }
 
   return (
     <main ref={container} className={styles.text_scroll_reveal_wrapper}>
-      <div className={styles.description}>
-        {splitWords(companyDesc)}
+      <div ref={description} className={styles.description}>
+        {
+          splitWords(companyDesc)
+        }
       </div>
     </main>
-  );
-};
-
-export default TextScrollReveal;
+  )
+}
